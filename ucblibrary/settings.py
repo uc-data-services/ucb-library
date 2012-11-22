c9b7a20426ae3fbe214afa9c17b435ee898b0d9d
Tim Dennis <dennis.tim@gmail.com>
2012-11-22 13:38:32 -0800

    initial commit for scrapy ucblib

---
+++ ucblibrary/settings.py
@@ -0,0 +1,15 @@
+# Scrapy settings for ucblibrary project
+#
+# For simplicity, this file contains only the most important settings by
+# default. All the other settings are documented here:
+#
+#     http://doc.scrapy.org/topics/settings.html
+#
+
+BOT_NAME = 'ucblibrary'
+
+SPIDER_MODULES = ['ucblibrary.spiders']
+NEWSPIDER_MODULE = 'ucblibrary.spiders'
+
+# Crawl responsibly by identifying yourself (and your website) on the user-agent
+#USER_AGENT = 'ucblibrary (+http://www.yourdomain.com)'
