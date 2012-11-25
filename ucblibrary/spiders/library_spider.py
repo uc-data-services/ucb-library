1b2d48600826292c735f9eb9643a53ee1b957a7d
Tim Dennis <dennis.tim@gmail.com>
2012-11-24 17:02:49 -0800

    adding spider

---
+++ ucblibrary/spiders/library_spider.py
@@ -0,0 +1,29 @@
+# coding: utf-8
+
+from scrapy.selector import HtmlXPathSelector
+from scrapy.contrib.spiders import CrawlSpider, Rule
+from scrapy.contrib.linkextractors.sgml import SgmlLinkExtractor 
+from scraper.items import MyItem ##This is the item I defined in items.py
+
+class MultiPagesSpider(CrawlSpider):
+	'''spiders lib hours, follows links'''
+	domain_name = 'lib.berkeley.edu'
+	allowed_domains = ['lib.berkeley.edu']
+	start_urls = ['http://www.lib.berkeley.edu/hours',]
+	rules = (Rule(SgmlLinkExtractor(allow=('/hours/front/info\?library_id=\d+', )), 
+		callback='parse_item', follow= True),)
+
+#/html/body/table/tbody/tr[2]/td/table/tbody/tr/td/table[2]/tbody/tr/td[4]/a
+#/html/body/table/tbody/tr[2]/td/table/tbody/tr/td/table[2]/tbody/tr[3]/td[4]/a
+
+	def parse_item(self, response):
+		hxs = HtmlXPathSelector(response)
+		items= hxs.select('/html/body/ul')
+		scraped_items =[]
+		for item in items:
+			scraped_item = MyItem() ### this is the item object you defined in the items file
+			scraped_item["title"] = item.select('li/text()').extract() ### assuming your item object has “title” field
+			scraped_items.append(scraped_item)
+		return(items)
+
+spider = MultiPagesSpider()
